162db9f8a4bb12af102143203ef0e22da7bee4b5
Dzhaman-Nazarii <dzhaman.nazar2003@gmail.com>
2023-07-21 10:44:32 +0300

    fixed mistakes

--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -6,7 +6,7 @@ import { deleteContact } from '../../redux/contactsSlice'
 export default function ContactList() {
   const dispatch = useDispatch()
 
-  const contacts = useSelector(state => state.contacts); // Переконайтеся, що ви користуєтеся ключем "contacts"
+  const contacts = useSelector(state => state.contacts);
   const filtration = useSelector(state => state.filter);
 
   const handleDelete = (idValue) => { dispatch(deleteContact(idValue)) };
